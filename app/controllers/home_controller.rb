08a5742246af50ca5d5186799bd96819a15d3ec0
Pedro Moreira <pedro@codecreations.tech>
2020-03-22 19:58:38 +0000

    Replace static form with rails form in homepage, tweak positioning of hero background

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,8 @@ class HomeController < ApplicationController
 
   def index
     @spinner_categories = Category.order("RANDOM()").limit(5)
-    @categories = Category.with_sellers
+    # @categories = Category.with_sellers
+    @categories = Category.all
     render :index, layout:'homepage'
   end
 
