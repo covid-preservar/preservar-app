4a32c45583cfc827dfae781894c8dae37a5ad19c
Pedro Moreira <pedro@codecreations.tech>
2020-03-23 15:05:40 +0000

    Search form with autocomplete widget

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,6 @@ class HomeController < ApplicationController
 
   def index
     @spinner_categories = Category.order("name ASC").limit(5)
-    # @categories = Category.with_sellers
-    @categories = Category.all
     gon.cities = Seller.distinct(:city).pluck(:city)
 
     render :index, layout:'homepage'
--- app/javascript/src/location_autocomplete.js
+++ app/javascript/src/location_autocomplete.js
@@ -1,11 +1,20 @@
 import "awesomplete";
 
 function setup() {
-  if ($('#location').length > 0 && !window.location_completer) {
-    window.location_completer = new Awesomplete($('#location')[0], { list: gon.cities })
+  if ($(".input-location").length > 0 && !window.location_completers) {
+    window.location_completers = []
+    $(".input-location").map((_, input) => {
+      window.location_completers.push(new Awesomplete(input, {list: gon.cities, minChars: 1}));
+    });
   }
+}
+
+function teardown() {
+  window.location_completers.forEach((completer) => completer.destroy());
+  window.location_completers = null;
 }
 
 document.addEventListener('DOMContentLoaded', setup, { once: true });
 document.addEventListener('turbolinks:load', setup, { once: true });
 document.addEventListener("turbolinks:render", setup);
+document.addEventListener("turbolinks:before-render", teardown);
