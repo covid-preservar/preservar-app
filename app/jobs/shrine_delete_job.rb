04a54d44aa5f503f01b9864d9f44e9d8f8827dac
Pedro Moreira <pedro@codecreations.tech>
2020-03-26 12:57:23 +0000

    Add missing shrine async jobs for uploads

---
+++ app/jobs/shrine_delete_job.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+class ShrineDeleteJob < ApplicationJob
+  queue_as :default
+
+  def perform(attacher_class, data)
+    attacher_class = Object.const_get(attacher_class)
+
+    attacher = attacher_class.from_data(data)
+    attacher.destroy
+  end
+end
---
+++ app/jobs/shrine_promote_job.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+class ShrinePromoteJob < ApplicationJob
+  queue_as :default
+
+  # attachment has changed or record has been deleted, nothing to do
+  discard_on Shrine::AttachmentChanged, ActiveRecord::RecordNotFound
+
+  def perform(attacher_class, record_class, record_id, name, file_data)
+    attacher_class = Object.const_get(attacher_class)
+    record = Object.const_get(record_class).find(record_id)
+
+    attacher = attacher_class.retrieve(model: record, name: name, file: file_data)
+    attacher.atomic_promote
+  end
+end
