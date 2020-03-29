5dfda992ebdb24e89510dea07dafbd9de08b8427
Pedro Moreira <pedro@codecreations.tech>
2020-03-29 16:41:59 +0100

    Update seller factory

--- spec/factories/seller.rb
+++ spec/factories/seller.rb
@@ -3,6 +3,8 @@ FactoryBot.define do
   factory :seller do
     name { 'Tasca do Chico' }
     category
-    payment_api_key { 'asdfgh123425' }
+    payment_api_key { Faker::Internet.uuid }
+    address { "#{Faker::Address.street_name}, #{Faker::Address.building_number}" }
+    area { Location.all.sample }
   end
 end
