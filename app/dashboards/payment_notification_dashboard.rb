9341cd47c5141e61a699d6892b712d0b53d6ba74
Pedro Moreira <pedro@codecreations.tech>
2020-03-31 11:41:48 +0100

    Code cleanup

--- app/dashboards/payment_notification_dashboard.rb
+++ app/dashboards/payment_notification_dashboard.rb
@@ -1,4 +1,5 @@
-require "administrate/base_dashboard"
+# frozen_string_literal: true
+require 'administrate/base_dashboard'
 
 class PaymentNotificationDashboard < Administrate::BaseDashboard
   # ATTRIBUTE_TYPES
@@ -21,28 +22,28 @@ class PaymentNotificationDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = %i[
-  id
-  data
-  status
-  created_at
+    id
+    data
+    status
+    created_at
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = %i[
-  id
-  data
-  status
-  created_at
-  updated_at
+    id
+    data
+    status
+    created_at
+    updated_at
   ].freeze
 
   # FORM_ATTRIBUTES
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = %i[
-  data
-  status
+    data
+    status
   ].freeze
 
   # COLLECTION_FILTERS
