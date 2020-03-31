e7a566d0669169a692669d0f0ea8fb91633a845e
Pedro Moreira <pedro@codecreations.tech>
2020-03-31 18:24:42 +0100

    Fix seller_user -> seller association scope

--- app/models/seller_user.rb
+++ app/models/seller_user.rb
@@ -5,6 +5,6 @@ class SellerUser < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  belongs_to :seller
+  belongs_to :seller, -> { unscoped }
   accepts_nested_attributes_for :seller
 end
