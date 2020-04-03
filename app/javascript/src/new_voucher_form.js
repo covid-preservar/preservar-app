9c40a09250d89b2e918b324f3306f6004f4ca664
Ricardo <ricardo@hardquarters.com>
2020-04-03 12:59:48 +0100

    CSS fixes to seller hero and share box

--- app/javascript/src/new_voucher_form.js
+++ app/javascript/src/new_voucher_form.js
@@ -1,11 +1,13 @@
 function setup() {
   if ($('#new_voucher').length > 0) {
     $('#voucher_custom_value').on('input', (ev) => {
+      $('#voucher_custom_value').parent().addClass("selected"); // I tried $(this), but didn't work
       $('.radio_buttons').prop('checked', false)
     })
 
     $('.radio_buttons').change( _ => {
-      $('#voucher_custom_value').val(null)
+      $('#voucher_custom_value').val(null);
+      $('#voucher_custom_value').parent().removeClass("selected");
     })
   }
 }
