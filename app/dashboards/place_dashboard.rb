8bb78f002ae0a9ba1376a0da85675d611d8a2899
Pedro Moreira <pedro@codecreations.tech>
2020-04-03 12:27:23 +0100

    Fix backoffice place edit form

--- app/dashboards/place_dashboard.rb
+++ app/dashboards/place_dashboard.rb
@@ -58,7 +58,7 @@ class PlaceDashboard < Administrate::BaseDashboard
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = %i[
-    seller_user
+    seller
     category
     name
     area
