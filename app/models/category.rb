d94931b7de643255a97ff6a9e677630faaece279
Pedro Moreira <pedro@codecreations.tech>
2020-04-07 11:26:58 +0100

    Fix bad category scope

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,6 +4,6 @@ class Category < ApplicationRecord
 
   validates :name, :name_plural, presence: true
 
-  scope :with_places, -> { where(id: Place.published.distinct(:category_id)) }
+  scope :with_places, -> { where(id: Place.published.distinct(:category_id).select(:category_id)) }
   default_scope -> { order(name: :asc) }
 end
