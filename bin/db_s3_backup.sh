14f98babb9b6d3d5ddc7361a7d0ed781e30ce0a0
Pedro Moreira <pedro@codecreations.tech>
2020-04-07 10:51:41 +0100

    Missing content from last commit

---
+++ bin/db_s3_backup.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# terminate script as soon as any command fails
+set -e
+
+if [[ -z "$APP" ]]; then
+  echo "Missing APP variable which must be set to the name of your app where the db is located"
+  exit 1
+fi
+
+if [[ -z "$DATABASE" ]]; then
+  echo "Missing DATABASE variable which must be set to the name of the DATABASE you would like to backup"
+  exit 1
+fi
+
+if [[ -z "$DB_S3_BUCKET_PATH" ]]; then
+  echo "Missing DB_S3_BUCKET_PATH variable which must be set the directory in s3 where you would like to store your database backups"
+  exit 1
+fi
+
+#install aws-cli
+curl https://s3.amazonaws.com/aws-cli/awscli-bundle.zip -o awscli-bundle.zip
+unzip awscli-bundle.zip
+chmod +x ./awscli-bundle/install
+./awscli-bundle/install -i /tmp/aws
+
+BACKUP_FILE_NAME="$(date +"%Y-%m-%d-%H-%M")-$APP-$DATABASE.dump"
+
+# heroku pg:backups capture $DATABASE --app $APP
+curl -o $BACKUP_FILE_NAME `heroku pg:backups:url --app $APP`
+FINAL_FILE_NAME=$BACKUP_FILE_NAME
+
+if [[ -z "$NOGZIP" ]]; then
+  gzip $BACKUP_FILE_NAME
+  FINAL_FILE_NAME=$BACKUP_FILE_NAME.gz
+fi
+
+/tmp/aws/bin/aws s3 cp $FINAL_FILE_NAME s3://$DB_S3_BUCKET_PATH/$APP/$DATABASE/$FINAL_FILE_NAME
+
+echo "backup $FINAL_FILE_NAME complete"
+
