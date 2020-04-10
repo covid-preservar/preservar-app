9a2c6c430d2581d84e8a251cb135976dc3f19e9b
Pedro Moreira <pedro@codecreations.tech>
2020-04-10 19:50:09 +0100

    Fix code loading issue when DB is not present

--- app/lib/subdomains.rb
+++ app/lib/subdomains.rb
@@ -4,17 +4,15 @@ module Subdomains
       attr_accessor :subdomains
     end
 
-    def self.loaded_subdomains
+    def self.subdomains
       return unless load_subdomains?
 
-      @loaded_subdomains ||= ::Partner.pluck(:slug)
+      @subdomains ||= ::Partner.pluck(:slug)
     end
 
     def self.load_subdomains?
       ::ActiveRecord::Base.connection.table_exists?("partners")
     end
-
-    @subdomains ||= loaded_subdomains
 
     def self.matches?(request)
       subdomains.include?(request.subdomain)
