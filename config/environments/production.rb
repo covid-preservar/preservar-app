a7ed0cd4d7695af6ff901c9052813566022e382c
Pedro Moreira <pedro@codecreations.tech>
2020-04-11 15:11:32 +0100

    Setup CORS for assets loaded from partner subdomains

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,6 +34,13 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   config.action_controller.asset_host = ENV['HOSTNAME']
 
+  config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins Regexp.new("^(.*\.|)#{ENV['HOSTNAME']}$")
+      resource '/assets/*', :headers => :any, :methods => [:get]
+    end
+  end
+
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
