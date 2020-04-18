d2448dfd4c871cf769b9e1932d76d0e9786728f0
Pedro Moreira <pedro@codecreations.tech>
2020-04-18 17:37:49 +0100

    Strip whitespace from VAT ID

--- app/models/seller.rb
+++ app/models/seller.rb
@@ -5,4 +5,12 @@ class Seller < ApplicationRecord
   has_many :vouchers, through: :places
 
   validates :vat_id, :contact_name, :company_name, presence: true
+
+  before_validation :sanitize_vat_id
+
+  private
+
+  def sanitize_vat_id
+    self.vat_id.tr!(' ', '')
+  end
 end
