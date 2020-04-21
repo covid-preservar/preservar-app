7ac89b4e77d0dbe1cc0b3647e915d71ca23f2043
Pedro Moreira <pedro@codecreations.tech>
2020-04-21 14:41:27 +0100

    Ensure place gets refreshed when partnership changs

--- app/models/partnership.rb
+++ app/models/partnership.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 class Partnership < ApplicationRecord
   belongs_to :partner, inverse_of: :partnerships
-  belongs_to :place, inverse_of: :partnership
+  belongs_to :place, inverse_of: :partnership, touch: true
   belongs_to :partner_identifier, optional: true
-
 end
