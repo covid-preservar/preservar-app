f38dfd143b11efcfdd7f3eb189f66961fe8ce1ff
Pedro Moreira <pedro@codecreations.tech>
2020-04-22 14:03:21 +0100

    Fix promo limit detection

--- app/models/place.rb
+++ app/models/place.rb
@@ -48,6 +48,12 @@ class Place < ApplicationRecord
     approved_partner if approved_partner&.active? && !partnership.limit_reached?
   end
 
+  def promo_limit_reached?
+    approved_partner.try(:voucher_limit).present? &&
+    approved_partner.voucher_limit > 0 &&
+    vouchers.paid.count >= approved_partner.voucher_limit
+  end
+
   def has_charity_partner?
     approved_partner.present? && partner.charity_partner?
   end
