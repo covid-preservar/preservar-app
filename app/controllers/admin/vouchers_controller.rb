c437d9c67bd8b43807a8efd4a9ff7df12bd52a87
Pedro Moreira <pedro@codecreations.tech>
2020-04-23 13:21:00 +0100

    Fix voucher index table contents

--- app/controllers/admin/vouchers_controller.rb
+++ app/controllers/admin/vouchers_controller.rb
@@ -7,8 +7,8 @@ module Admin
         {attr: :id, label: 'ID', sort: :id},
         {attr: :place, label: 'Place', sort: nil, formatter: -> (view, voucher) { view.link_to "Place: #{voucher.place.name}", [:admin, voucher.place] } },
         {attr: :email, label: 'Email', sort: :email },
-        {attr: :value, label: 'Value', sort: :value},
-        {attr: :state, label: 'State', sort: :state, formatter: -> (view, voucher) { view.number_to_currency voucher.value, locale: :pt, precision: 0 }},
+        {attr: :value, label: 'Value', sort: :value, formatter: -> (view, voucher) { view.number_to_currency voucher.value, locale: :pt, precision: 0 }},
+        {attr: :state, label: 'State', sort: :state},
         {attr: :created_at, label: 'Created At', sort: :created_at}
       ]
     end
