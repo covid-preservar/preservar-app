b4ae5fa133213889fa7c542e7f313032e6896741
Pedro Moreira <pedro@codecreations.tech>
2020-04-30 16:54:58 +0100

    Add VAT ID to insurance parameters

--- app/services/insurance_policy_service.rb
+++ app/services/insurance_policy_service.rb
@@ -20,6 +20,7 @@ class InsurancePolicyService
       email: voucher.email,
       amount: voucher.value,
       retailer: voucher.place.name,
+      nif: voucher.place.seller.vat_id,
       purchase_date: voucher.payment_completed_at.to_date,
       expiry_date: DEFAULT_INSURANCE_EXPIRATION
     }
